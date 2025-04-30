402a48821b53198eb11d1a6c3f17d2b28d0f3d69
IPVIASATELITE <ipviasatelite@gmail.com>
2025-04-30 12:33:37 -0600

    Add files via upload

---
+++ index_htm_files/simpleGallery_5cols.js
@@ -0,0 +1,10 @@
+function xaraSwidgets_simpleGallery5ColumnsConstructor(divID, data)
+{
+	xaraSwidgets_simpleGalleryOverallConstructor(divID, data, {
+		
+		cols:5,
+		padding:4,
+		margin:2
+		
+	});
+}
