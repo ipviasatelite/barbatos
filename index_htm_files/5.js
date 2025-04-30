fe4d2f0ad1d432a91efbc192933dc2a2698c3110
IPVIASATELITE <ipviasatelite@gmail.com>
2025-04-30 12:32:31 -0600

    Add files via upload

---
+++ index_htm_files/5.js
@@ -0,0 +1,13 @@
+window.local_mosaicBlockBar1_1_data = [
+{
+ direction: "0",
+ speed: "0.5",
+ theme: "0"
+},
+{
+ heading: "index_htm_files/141.png",
+ image: "index_htm_files/142.jpg",
+ link: "http://www.xara.com",
+ text: "index_htm_files/143.png"
+}
+];
