82addc6240e2e412c7b3ae4f1b25e6ab2d403cac
IPVIASATELITE <ipviasatelite@gmail.com>
2025-04-30 12:22:52 -0600

    Add files via upload

---
+++ index_htm_files/3.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery5Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/62.jpg",
+ thumbnail: "index_htm_files/63.jpg"
+},
+{
+ image: "index_htm_files/64.jpg",
+ thumbnail: "index_htm_files/65.jpg"
+},
+{
+ image: "index_htm_files/66.jpg",
+ thumbnail: "index_htm_files/67.jpg"
+},
+{
+ image: "index_htm_files/68.jpg",
+ thumbnail: "index_htm_files/69.jpg"
+},
+{
+ image: "index_htm_files/70.jpg",
+ thumbnail: "index_htm_files/71.jpg"
+},
+{
+ image: "index_htm_files/72.jpg",
+ thumbnail: "index_htm_files/73.jpg"
+},
+{
+ image: "index_htm_files/74.jpg",
+ thumbnail: "index_htm_files/75.jpg"
+},
+{
+ image: "index_htm_files/76.jpg",
+ thumbnail: "index_htm_files/77.jpg"
+}
+];
---
+++ index_htm_files/4.js
@@ -0,0 +1,23 @@
+window.local_cycleBottomradio_v8_1_data = [
+{
+ bulletimage: "index_htm_files/6.png",
+ image: "index_htm_files/7.jpg",
+ link: "#"
+},
+{
+ image: "index_htm_files/8.jpg",
+ link: "#"
+},
+{
+ image: "index_htm_files/9.jpg",
+ link: "#"
+},
+{
+ image: "index_htm_files/10.jpg",
+ link: "#"
+},
+{
+ image: "index_htm_files/11.jpg",
+ link: "#"
+}
+];
